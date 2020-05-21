4d97576eed6b28130b06588c8d99ab639bb85b0f
Joalbert <joalbertgonzalez@gmail.com>
2020-05-21 11:17:44 -0500

    [ADD] pluglis webpack added

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const HtmlWebpack = require('html-webpack-plugin');
+const copyWebpack = require('copy-webpack-plugin');
+const HtmlWebpackTags = require('html-webpack-tags-plugin');
 
 module.exports = {
   entry: "./src/main.js",
@@ -6,6 +9,18 @@ module.exports = {
   output: {
     path: path.join(__dirname, "build"),
     filename: "fixter.js"
-  }
+  },
+  plugins: [
+    new HtmlWebpack({
+      filename: 'index.html',
+      template: 'public/index.html'
+    }),
+    new copyWebpack({
+      patterns: [{ from: 'public/assets/', to: 'css/' }],
+    }),
+    new HtmlWebpackTags({
+      links: ['css/styles.css']
+    })
+  ]
 }
 
